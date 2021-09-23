198222252807848937c20f34eaae4a02fd945898
[shivani tamboli] <[tambolishivani4@gmail.com]>
2021-09-23 08:43:30 +0530

    Added : create board to assign empty space to each elements

--- src/com/workshop1/TicTacToe.java
+++ src/com/workshop1/TicTacToe.java
@@ -2,8 +2,19 @@ package com.workshop1;
 
 public class TicTacToe {
 
+		    static char[] board = new char[10]; // board size 10
 		    public static void main(String[] args) {
+		        createBoard();//calling method
 		        System.out.printf("Welcome to Tic Tac Toe Game");
 		    }
-		    
+		    /*why this Method use:
+		     *in method data is character
+		     *and using for loop to iteration from 1 to 9 by ignoring 0
+		     */
+		    public static void createBoard() {
+		        for (int index = 1; index < board.length; index++) {
+		            board[index] = ' ';
+		        }
+	}
+
 }
