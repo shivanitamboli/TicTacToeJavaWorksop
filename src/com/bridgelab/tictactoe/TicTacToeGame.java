9783cd9023b06be56c6fb5daa5d204e41aed276b
[shivani tamboli] <[tambolishivani4@gmail.com]>
2021-09-23 14:00:53 +0530

    Added : Create method of showBoard

--- src/com/bridgelab/tictactoe/TicTacToeGame.java
+++ src/com/bridgelab/tictactoe/TicTacToeGame.java
@@ -4,23 +4,24 @@ import java.util.Scanner;
 
 public class TicTacToeGame {
 	public static char playerSymbol;
-	public static char computer_Player_Symbol;
+	public static char cpSymbol;
 	static char[] board = new char[10]; // board size 10
 
 	public static void main(String[] args) {
 		createBoard();// calling method
 		System.out.println(
-				"Select the Letter which you want\n  Press 1 . Choose X \n  Press 2 . Choose O\n\nEnter your Choice : ");
+				"Select the Letter you wanted \n  Press 1 . Choose X \n  Press 2 . Choose O\n\nEnter your Choice : ");
 		Scanner scan = new Scanner(System.in); // Create scanner object
 		int choice = scan.nextInt();
 
 		TicTacToeGame.allowPlayer(choice);
+		TicTacToeGame.showBoard();
 		scan.close();
 	}
 
 	/*
-	 * why Method use: in method data is character and using for loop to iteration
-	 * from 1 to 9
+	 * Method use: in method data is character and using for loop to iteration from
+	 * 1 to 9
 	 */
 	public static void createBoard() {
 		for (int index = 1; index < board.length; index++) {
@@ -29,27 +30,30 @@ public class TicTacToeGame {
 
 	}
 
-	/*
-	 * define method to allow the player in tictactoe game and by using switch which
-	 * u want to choice you can directly choice it
-	 */
 	public static void allowPlayer(int choice) {
 		switch (choice) {
 		case 1: {
 			playerSymbol = 'X';
-			computer_Player_Symbol = 'O';
+			cpSymbol = 'O';
 			break;
 		}
 		case 2: {
 			playerSymbol = 'O';
-			computer_Player_Symbol = 'X';
+			cpSymbol = 'X';
 			break;
 		}
 		default: {
-			System.out.println("Invalid Choice so please Retry Again");
+			System.out.println("Invalid Choice . Retry Again!");
 			return;
 		}
 		}
+	}
+
+	public static void showBoard() {
+		System.out.println("Board Display\n");
+		System.out.println(" | " + board[1] + " | " + board[2] + " | " + board[3] + " |\n");
+		System.out.println(" | " + board[4] + " | " + board[5] + " | " + board[6] + " |\n");
+		System.out.println(" | " + board[7] + " | " + board[8] + " | " + board[9] + " |\n");
 	}
 
 }
